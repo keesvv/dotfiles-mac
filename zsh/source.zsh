8863b7ec64483a290b29354aaf8014a9b9a5b3b5
Kees van Voorthuizen <me@keesvv.nl>
2023-02-22 11:51:50 +0100

    feat: add file mgmt & git aliases

--- zsh/source.zsh
+++ zsh/source.zsh
@@ -3,3 +3,21 @@ export EDITOR=nvim
 
 # Aliases
 alias vi=$EDITOR
+
+## File management
+alias ls='ls --color'
+alias l='ls -lA'
+
+## Git
+alias gs='git status'
+alias gb='git branch'
+alias gc='git commit'
+alias gp='git push'
+alias ga='git add'
+alias gd='git diff'
+alias gf='git fetch -p'
+alias gl='git log'
+alias gr='git restore'
+alias grs='git restore --staged'
+alias glo='git log --oneline'
+alias gco='git checkout'
