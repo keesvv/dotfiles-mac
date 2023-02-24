48a38a15286cd9ea05e3024ffce6091edda07b5a
Kees van Voorthuizen <me@keesvv.nl>
2023-02-24 23:49:11 +0100

    feat: prompt tweaks & fish_greeting

---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,5 @@
+function fish_greeting
+    if type -q fortune
+        fortune -s wisdom
+    end
+end
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,6 @@
 function fish_prompt -d "Write out the prompt"
-    printf '%s@%s %s%s $ ' $USER (prompt_hostname) (prompt_pwd -D 2) (fish_git_prompt)
+    set_color -d
+    printf '%s@%s %s%s $ ' \
+        $USER (prompt_hostname) (prompt_pwd -D 2) (fish_git_prompt)
+    set_color normal
 end
